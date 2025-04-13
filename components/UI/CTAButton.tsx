ba2d5973d5d5eb272a698aabfdf0f3bca21ca182
hannahchapman79 <hannahchapman1999@aol.com>
2025-04-13 11:55:22 +0700

    Fix hydration HTML error

--- components/UI/CTAButton.tsx
+++ components/UI/CTAButton.tsx
@@ -8,9 +8,8 @@ interface CTAButtonProps {
 
 export default function CTAButton({ text, link }: CTAButtonProps) {
   return (
-    <Link href={link} passHref>
+    <Link href={link}>
       <Button
-        as="a"
         className="text-lg md:text-xl px-6 py-3 md:px-8 md:py-4 font-semibold bg-gradient-to-r from-indigo-900 via-purple-800 to-emerald-700 text-white shadow-xl hover:shadow-2xl transition-all duration-300"
         radius="full"
       >
