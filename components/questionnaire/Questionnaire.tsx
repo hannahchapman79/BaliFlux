435e4ca7899c83255fcefa4efca71793ab07ba33
hannahchapman79 <hannahchapman1999@aol.com>
2025-05-08 19:10:59 +0700

    Make button size responsive

--- components/questionnaire/Questionnaire.tsx
+++ components/questionnaire/Questionnaire.tsx
@@ -164,7 +164,7 @@ export default function Questionnaire({
                 <button
                   type="submit"
                   disabled={isSubmitting}
-                  className="flex items-center px-6 py-2.5 bg-gradient-to-r from-teal-500 to-green-500 text-white rounded-lg hover:from-teal-400 hover:to-green-400 transition shadow-md disabled:opacity-70 disabled:cursor-wait"
+                  className="flex items-center px-5 py-2.5 bg-gradient-to-r from-teal-500 to-green-500 text-sm text-white rounded-md hover:from-teal-400 hover:to-green-400 transition shadow-md disabled:opacity-70 disabled:cursor-wait"
                 >
                   {isSubmitting ? (
                     <>
